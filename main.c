46360115822a520583dd66ce207a2c9ba2f43b3f
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-05-28 21:20:54 -0700

    bit

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 // Author:              Sandro Aguilar
 // Date:                May 28, 2019
-// Class:               CS 261 Oregon State Un
+// Class:               CS 261 Oregon State Uni
 
 #include <stdio.h>
 
