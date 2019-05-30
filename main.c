77866a8dd132d50070fa707e838a969e8982d89e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-05-29 22:11:29 -0700

    bit

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // Author:              Sandro Aguilar
 // Date:                May 28, 2019
 // Class:               CS 261 Oregon State University
-// Description:         testing bitwise operato
+// Description:         testing bitwise operator
 
 #include <stdio.h>
 
