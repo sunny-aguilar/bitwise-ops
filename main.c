ada09ad259d3eb89d2fc0589b5d516e79711ff50
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-05-29 22:10:28 -0700

    bit

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // Author:              Sandro Aguilar
 // Date:                May 28, 2019
 // Class:               CS 261 Oregon State University
-// Description:         testing bitw
+// Description:         testing bitwi
 
 #include <stdio.h>
 
