d8c765a0b496c7c6bd8f9b413231b5c5f40d5bda
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-03 23:11:38 -0700

    a5

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 // Date:                May 28, 2019
 // Class:               CS 261 Oregon State University
 // Description:         testing bitwise operators; bitwise AND operator for
-//                      finding an odd number us
+//                      finding an odd number usi
 
 #include <stdio.h>
 
