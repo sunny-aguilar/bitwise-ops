ad1f39d1e99c5af373990d8d77b7c3de6a2bd190
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-07 20:46:28 -0700

    a5

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 // Class:               CS 261 Oregon State University
 // Description:         testing bitwise operators; bitwise AND operator for
 //                      finding an odd number using bitwise operators and using the and operator
-//                      including other bitwise operators to test. Bitwise opera
+//                      including other bitwise operators to test. Bitwise operat
 
 #include <stdio.h>
 
